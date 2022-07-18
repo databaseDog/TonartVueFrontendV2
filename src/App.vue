a1faf8ec27f2205e273f9e1ff2e5a765c314166e
Martin Kuen <mail@martinkuen.de>
2022-07-18 16:11:59 +0200

    Neues projekt

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div class="max-w-6xl">
+    <div class="flex py-4 items-center justify-between">
+      <h1 class="flex h-12 uppercase mr-8 text-3xl items-center font-light">
+        <img src="./assets/header-icons.png" class="h-12 mr-2" />
+        Tonart Aachen
+      </h1>
+      <div class="flex space-x-8 uppercase font-light items-center">
+        <div class="hover:text-green-800 cursor-pointer">Noten</div>
+        <div class="hover:text-green-800 cursor-pointer">Proben</div>
+        <div class="hover:text-green-800 cursor-pointer">Umfragen</div>
+        <div class="hover:text-green-800 cursor-pointer">Dateien</div>
+        <div class="hover:text-green-800 cursor-pointer">Boards</div>
+        <div class="hover:text-green-800 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+        </svg></div>
+        <div class="hover:text-green-800 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+        </svg></div>
+        <div class="hover:text-green-800 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+        </svg></div>
+      </div>
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dolore
+      magnam modi obcaecati quam ratione tempora vero? Alias, amet debitis, eos
+      fugiat, id illo officia quisquam sapiente vel veniam voluptatum!
+    </div>
+  </div>
+</template>
